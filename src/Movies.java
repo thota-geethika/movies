75a10154c2fabc15e2b526d5966f04ff79de0744
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-29 11:00:22 +0530

    Add. Included the logic to implement movie class and object..

--- src/Movies.java
+++ src/Movies.java
@@ -1,5 +1,43 @@
+
+import java.util.ArrayList;
+
+class movie {
+
+    private final String title;
+    private final String studio;
+    private final String rating;
+
+    movie(String title, String studio, String rating)
+    {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+
+    movie(String title, String studio)
+    {
+        this.title = title;
+        this.studio = studio;
+        rating = "PG";
+    }
+
+    ArrayList<String> getPg(ArrayList<movie> moviesInstances)
+    {
+        ArrayList<String> movieNames = new ArrayList<>();
+        for(movie iterator : moviesInstances)
+        {
+            if(iterator.rating.equals("PG"))
+            {
+                movieNames.add(iterator.title);
+            }
+        }
+        return movieNames;
+    }
+
+}
+
 public class Movies {
     public static void main(String[] args) {
-
+        movie film1 = new movie("Casino Royale","Eon Productions", "PG-13");
     }
 }
